ac3d1b05cb29954944f9b157dee0d00abde6b528
AltonTravers <69270642+AltonTravers@users.noreply.github.com>
2021-06-10 13:03:43 +0800

    NAVBAR animation

--- protfolio202101/src/components/RBNavbar.tsx
+++ protfolio202101/src/components/RBNavbar.tsx
@@ -1,26 +1,24 @@
-import React, {useState} from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import styled from 'styled-components';
-import { Envelope } from 'react-bootstrap-icons';
-import './RBNavbar.css';
+import React, { useState } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import styled from 'styled-components'
+import { Envelope } from 'react-bootstrap-icons'
+import './RBNavbar.css'
 
 const NavIcon = styled(Nav.Link)`
     font-size: 14px;
-    color: #fff!important;
+    color: #fff !important;
     font-weight: bold;
     &:hover svg {
         color: #fff;
         transition: all 0.2s;
-
     }
 `
 
 const StyledEnvelop = styled(Envelope)`
     color: #f7ac5d;
     transition: all 0.2s;
-
 `
 
 const StyledNavDropdownItem = styled(NavDropdown.Item)`
@@ -29,7 +27,7 @@ const StyledNavDropdownItem = styled(NavDropdown.Item)`
 
 const NavLink = styled(Nav.Link)`
     font-size: 14px;
-    color: #fff!important;
+    color: #fff !important;
     font-weight: bold;
 
     @media (min-width: 992px) {
@@ -48,7 +46,7 @@ const NavLink = styled(Nav.Link)`
             height: 1px;
             transition: all 0.4s;
             transform: scaleX(0);
-            transform-origin: left!important;
+            transform-origin: left !important;
         }
         &:hover:after {
             transform: scaleX(1);
@@ -93,40 +91,88 @@ const StyledNavDropdown = styled(NavDropdown)`
 }
 `
 
-
 const StyledNavbar = styled(Navbar)`
-    background: #415d6b!important;
+    background: #415d6b !important;
 `
 
 const RBNavbar = () => {
-    const [isDropdownShown, setIsDropdownShown] = useState<boolean>(false);
+    const [isDropdownShown, setIsDropdownShown] = useState<boolean>(false)
 
     const onHoverDropdown = (boo: boolean) => {
-        setIsDropdownShown(boo);
+        setIsDropdownShown(boo)
     }
 
     return (
         <div>
-            <StyledNavbar collapseOnSelect expand="xl" bg="dark" variant="dark" className="mx-auto py-3">
-                <Navbar.Brand href="#home"className="d-block d-xl-none mx-3 py-3 " >React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav navbar-expand-md" className="navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className=" justify-content-center">
+            <StyledNavbar
+                collapseOnSelect
+                expand="xl"
+                bg="dark"
+                variant="dark"
+                className="mx-auto py-3"
+            >
+                <Navbar.Brand
+                    href="#home"
+                    className="d-block d-xl-none mx-3 py-3 "
+                >
+                    React-Bootstrap
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav navbar-expand-md"
+                    className="navbar-nav"
+                />
+                <Navbar.Collapse
+                    id="responsive-navbar-nav"
+                    className=" justify-content-center"
+                >
                     <Nav className="col-lg-5 me-auto p-0 h-100">
-                        <NavLink href="#features" className="py-3 px-0 mx-3">Menu</NavLink>
-                        <NavLink href="#features" className="py-3 px-0 mx-3">Faq</NavLink>
-                        <NavLink href="#pricing" className="py-3 px-0 mx-3">Uber Eats</NavLink>
+                        <NavLink href="#features" className="py-3 px-0 mx-3">
+                            Menu
+                        </NavLink>
+                        <NavLink href="#features" className="py-3 px-0 mx-3">
+                            Faq
+                        </NavLink>
+                        <NavLink href="#pricing" className="py-3 px-0 mx-3">
+                            Uber Eats
+                        </NavLink>
                     </Nav>
-                    <Navbar.Brand href="#home" className="d-none d-xl-block mx-auto text-center col-auto py-3">React-Bootstrap</Navbar.Brand>
-                    <Nav className="col-lg-5 justify-content-end p-0"> 
-                        <StyledNavDropdown title="Dropdown" id="nav-dropdown"  className="py-3 px-0 mx-3" onMouseEnter={() => onHoverDropdown(true)} onMouseLeave={() => onHoverDropdown(false)} show={isDropdownShown}>
-                            <StyledNavDropdownItem eventKey="4.1">Action</StyledNavDropdownItem>
-                            <StyledNavDropdownItem eventKey="4.2">Another action</StyledNavDropdownItem>
-                            <StyledNavDropdownItem eventKey="4.3">Something else here</StyledNavDropdownItem>
+                    <Navbar.Brand
+                        href="#home"
+                        className="d-none d-xl-block mx-auto text-center col-auto py-3"
+                    >
+                        React-Bootstrap
+                    </Navbar.Brand>
+                    <Nav className="col-lg-5 justify-content-end p-0">
+                        <StyledNavDropdown
+                            title="Dropdown"
+                            id="nav-dropdown"
+                            className="py-3 px-0 mx-3"
+                            onMouseEnter={() => onHoverDropdown(true)}
+                            onMouseLeave={() => onHoverDropdown(false)}
+                            show={isDropdownShown}
+                        >
+                            <StyledNavDropdownItem eventKey="4.1">
+                                Action
+                            </StyledNavDropdownItem>
+                            <StyledNavDropdownItem eventKey="4.2">
+                                Another action
+                            </StyledNavDropdownItem>
+                            <StyledNavDropdownItem eventKey="4.3">
+                                Something else here
+                            </StyledNavDropdownItem>
                         </StyledNavDropdown>
-                        <NavLink href="#deets" className="py-3 px-0 mx-3">About Us</NavLink>
-                        <NavLink href="#memes" className="py-3 px-0 mx-3">Blog</NavLink>
-                        <NavLink href="#memes" className="py-3 px-0 mx-3">Contact Us</NavLink>
-                        <NavIcon href="#memes" className="py-3 px-0 mx-3"><StyledEnvelop size={20} /></NavIcon>
+                        <NavLink href="#deets" className="py-3 px-0 mx-3">
+                            About Us
+                        </NavLink>
+                        <NavLink href="#memes" className="py-3 px-0 mx-3">
+                            Blog
+                        </NavLink>
+                        <NavLink href="#memes" className="py-3 px-0 mx-3">
+                            Contact Us
+                        </NavLink>
+                        <NavIcon href="#memes" className="py-3 px-0 mx-3">
+                            <StyledEnvelop size={20} />
+                        </NavIcon>
                     </Nav>
                 </Navbar.Collapse>
             </StyledNavbar>
